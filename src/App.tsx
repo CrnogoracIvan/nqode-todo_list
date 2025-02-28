42e91c90d20bb9e24c46edf22dee2f54ed197c47
Ivan Crnogorac <ivan.crnogorac@rubikscode.net>
2025-02-28 15:47:44 +0100

    New item form moved to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,54 @@
-
 import {useState} from "react";
 import {NewItemButton} from "./components/NewItemButton/NewItemButton.tsx";
 import {Item} from "./components/Item/Item.tsx";
 import {NewItemForm} from "./components/NewItemForm/NewItemForm.tsx";
-
-// interface IItem {
-//     id: string;
-//     title: string;
-//     description: string;
-//     dueDate: string;
-// }
+import {IListItem} from "./types.ts";
 
 function App() {
 
     const [isNewItemFormVisible, setIsNewItemFormVisible] = useState<boolean>(false);
+    const [list, setList] = useState<IListItem[]>([]);
 
     const handleNewItemClick = () => {
         setIsNewItemFormVisible(true);
+    }
+
+    const handleSubmit = (item: IListItem) => {
+        const newList = [...list];
+        newList.push(item);
+        setList(newList);
     }
 
     const renderLeftSideTitle = () => (
         <h1 className='text-7xl font-bold mb-6'>Today</h1>
     )
 
-  return (
-    <>
-      <p className='text-3xl font-bold text-blue-600'>
-        NQcode To Do List
-      </p>
-        <div className='flex flex-row justify-center w-full'>
-            <div className='flex flex-row justify-center w-1/2'>
-                <div className={'w-1/2 pr-4'}>
-                    {renderLeftSideTitle()}
-                    <NewItemButton onClick={handleNewItemClick} />
-                    <Item itemName={'item1'}/>
-                </div>
-                <div className={'bg-slate-100 w-1/2 border-1 rounded-lg'}>
-                    {isNewItemFormVisible ? <NewItemForm/> : <p>right side</p>}
+    const renderItemList = (items: IListItem[]) => {
+        return items.map((item) => (
+            <Item itemName={item.title}/>
+        ))
+    }
+
+
+    return (
+        <>
+            <p className='text-3xl font-bold text-blue-600'>
+                NQcode To Do List
+            </p>
+            <div className='flex flex-row justify-center w-full'>
+                <div className='flex flex-row justify-center w-1/2'>
+                    <div className={'w-1/2 pr-4'}>
+                        {renderLeftSideTitle()}
+                        <NewItemButton onClick={handleNewItemClick}/>
+                        {renderItemList(list)}
+                    </div>
+                    <div className={'bg-slate-50 w-1/2 border-1 rounded-lg'}>
+                        {isNewItemFormVisible ? <NewItemForm onSubmit={handleSubmit}/> : <p>right side</p>}
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default App
--- src/components/NewItemForm/NewItemForm.tsx
+++ src/components/NewItemForm/NewItemForm.tsx
@@ -1,7 +1,51 @@
-export const NewItemForm = () => {
+import {useState} from "react";
+import {IListItem} from "../../types.ts";
+
+
+interface IProps {
+    onSubmit: (item: IListItem) => void;
+}
+
+
+export const NewItemForm = ({onSubmit}: IProps) => {
+
+    const [title, setTitle] = useState<string>('');
+    const [description, setDescription] = useState<string>('');
+
+    const disableSubmit = title.length === 0 || description.length === 0;
+
+    const renderInput = (title: string, value: string, onChangeValue: (newValue: string) => void) => (
+        <div className="w-full  min-w-[200px] py-2">
+            <input
+                className="w-full bg-transparent placeholder:text-slate-500 text-slate-700 text-sm border border-slate-400 rounded-md px-3 py-2 transition duration-300 ease focus:border-amber-300 focus:outline-none shadow-sm focus:shadow"
+                placeholder={`Add ${title} ...`}
+                value={value}
+                onChange={(e) => onChangeValue(e.target.value)}
+            />
+        </div>
+    )
+
+    const renderConfirmButton = () => (
+        <button
+            className="rounded-md bg-amber-300 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg hover:bg-amber-500 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none w-48"
+            type="button"
+            disabled={disableSubmit}
+            onClick={() => onSubmit({
+                title,
+                description,
+            })}
+        >
+            Save item
+        </button>
+    )
+
     return (
-        <div className={'flex flex-1 border-2 w-full h-full'}>
-            nja
+        <div className={'flex flex-1 flex-col border-1 w-full p-4'}>
+            {renderInput('Title', title, setTitle)}
+            {renderInput('Description', description, setDescription)}
+            <div className="flex flex-1 justify-end">
+                {renderConfirmButton()}
+            </div>
         </div>
     )
 }
